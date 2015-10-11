1f1fb3971d578f3bfc4a81d442e67cc48811a57f
Chris Liu <liugh@rippletek.com>
2015-10-11 16:01:28 +0000

    first working ver

--- markov.go
+++ markov.go
@@ -3,8 +3,14 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"strings"
+	"math/rand"
+	"os"
+	"regexp"
+	"strconv"
+	"time"
 )
+
+const NOWORD string = " "
 
 func insertSuffix(tab map[string][]string, key string, suffix string) {
 	if v, found := tab[key]; found {
@@ -19,18 +25,17 @@ func buildKey(prefix1 string, prefix2 string) string {
 }
 
 func BuildIndice(text string) map[string][]string {
-	arr := strings.Split(strings.Trim(text, " \n"), " ")
-	if len(arr) > 2 {
-		lim := len(arr) - 2
-		i := 0
-		tab := make(map[string][]string, 10240)
-		for i < lim {
-			insertSuffix(tab, buildKey(arr[i], arr[i+1]), arr[i+2])
-			i++
-		}
-		return tab
+	words := regexp.MustCompile("\\w+")
+	arr := words.FindAllString(text, -1)
+	prefix1, prefix2, suffix := NOWORD, NOWORD, NOWORD
+	tab := make(map[string][]string, 10240)
+	for i := range arr {
+		prefix1 = prefix2
+		prefix2 = suffix
+		suffix = arr[i]
+		insertSuffix(tab, buildKey(prefix1, prefix2), suffix)
 	}
-	return nil
+	return tab
 }
 
 func check(e error) {
@@ -39,8 +44,33 @@ func check(e error) {
 	}
 }
 
+func choose(suffix []string) string {
+	return suffix[rand.Intn(len(suffix))]
+}
+
+func generate(prefixes map[string][]string, n int) {
+	prefix1, prefix2 := NOWORD, NOWORD
+	rand.Seed(time.Now().UTC().UnixNano())
+	num_words := 0
+	for num_words < n {
+		if suffix, found := prefixes[buildKey(prefix1, prefix2)]; found {
+			word := choose(suffix)
+			fmt.Printf("%s ", word)
+			prefix1 = prefix2
+			prefix2 = word
+		} else {
+			return
+		}
+		num_words++
+	}
+}
+
 func main() {
-	text, err := ioutil.ReadFile("psalm.txt")
+	filename := os.Args[1]
+	count, err := strconv.Atoi(os.Args[2])
 	check(err)
-	BuildIndice(string(text))
+	text, err := ioutil.ReadFile(filename)
+	check(err)
+	prefixes := BuildIndice(string(text))
+	generate(prefixes, count)
 }
--- markov_test.go
+++ markov_test.go
@@ -7,8 +7,11 @@ import (
 
 func TestBuildIndice(t *testing.T) {
 	result := BuildIndice("a b")
-	assert.Equal(t, map[string][]string(nil), result)
+	assert.Equal(t, []string{"a"}, result["   "])
 
 	result = BuildIndice("a b c")
 	assert.Equal(t, []string{"c"}, result["a b"])
+
+	result = BuildIndice("a b c a b d")
+	assert.Equal(t, []string{"c", "d"}, result["a b"])
 }
