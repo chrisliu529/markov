840e4478b4f1ba2e1fea98b292499de7e5290292
Chris Liu <liugh@rippletek.com>
2015-10-12 06:55:23 +0000

    add elapsed time report and build script

---
+++ build.sh
@@ -0,0 +1,1 @@
+go build && time ./markov psalm.txt 10000 > markov.txt
--- markov.go
+++ markov.go
@@ -5,8 +5,8 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"os"
-	"regexp"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -25,8 +25,9 @@ func buildKey(prefix1 string, prefix2 string) string {
 }
 
 func BuildIndice(text string) map[string][]string {
-	words := regexp.MustCompile("\\w+")
-	arr := words.FindAllString(text, -1)
+	t := time.Now().UTC().UnixNano()
+	arr := strings.Fields(text)
+	t = report_elapse(t, "split words")
 	prefix1, prefix2, suffix := NOWORD, NOWORD, NOWORD
 	tab := make(map[string][]string, 10240)
 	for i := range arr {
@@ -35,6 +36,7 @@ func BuildIndice(text string) map[string][]string {
 		suffix = arr[i]
 		insertSuffix(tab, buildKey(prefix1, prefix2), suffix)
 	}
+	report_elapse(t, "build hash")
 	return tab
 }
 
@@ -65,12 +67,22 @@ func generate(prefixes map[string][]string, n int) {
 	}
 }
 
+func report_elapse(t int64, title string) int64 {
+	t2 := time.Now().UTC().UnixNano()
+	fmt.Fprintf(os.Stderr, "%s elapsed = %d ns\n", title, t2-t)
+	return t2
+}
+
 func main() {
 	filename := os.Args[1]
 	count, err := strconv.Atoi(os.Args[2])
 	check(err)
+	t := time.Now().UTC().UnixNano()
 	text, err := ioutil.ReadFile(filename)
 	check(err)
+	t = report_elapse(t, "read file")
 	prefixes := BuildIndice(string(text))
+	t = report_elapse(t, "build indice")
 	generate(prefixes, count)
+	t = report_elapse(t, "generate output")
 }
